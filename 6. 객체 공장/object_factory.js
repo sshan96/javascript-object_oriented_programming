486bb19d0e0452cb29ebdb7d4b3806b1aad4b915
sshan <sshan96@naver.com>
2024-09-19 17:12:01 +0900

    6.3 constructor 만들기

--- 6. 객체 공장/object_factory.js
+++ 6. 객체 공장/object_factory.js
@@ -1,24 +1,22 @@
-var kim = {
-    name: 'kim',
-    first: 10,
-    second: 20,
-    third: 30,
-    sum: function() {
+function Person(name, first, second, third) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+    this.third = third;
+    this.sum = function() {
         return this.first + this.second + this.third;
     }
 }
-var lee = {
-    name: 'lee',
-    first: 10,
-    second: 10,
-    third: 10,
-    sum: function() {
-        return this.first + this.second + this.third;
-    }
-}
+
+var kim = new Person('kim', 10, 20, 30);
+var lee = new Person('lee', 10, 10, 10);
 console.log("kim.sum()", kim.sum());
 console.log("lee.sum()", lee.sum());
 
 var d1 = new Date('2024-09-19');
 console.log("d1.getFullYear()", d1.getFullYear());
 console.log("d1.getMonth()", d1.getMonth());
+
+console.log('Date', Date);
+
+console.log('Person()', Person());
