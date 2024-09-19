61e26f65c327b2f7aa14b525a612b93675d4d23e
sshan <sshan96@naver.com>
2024-09-19 17:40:04 +0900

    8. Classes 의 생성

---
+++ 8. class/class.js
@@ -0,0 +1,14 @@
+class Person {
+
+}
+
+var kim = new Person();
+console.log('kim', kim);
+
+/* var kim = new Person('kim', 10, 20);
+kim.sum = function() {
+    return 'this : ' + (this.first + this.second);
+}
+var lee = new Person('lee', 10, 10); 
+console.log("kim.sum()", kim.sum());
+console.log("lee.sum()", lee.sum()); */
