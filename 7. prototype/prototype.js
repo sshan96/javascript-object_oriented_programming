188f933cb124430190a24c1375a089a6f18543b9
sshan <sshan96@naver.com>
2024-09-19 17:28:52 +0900

    7.2 prototype을 이용해서 재사용성을 높이기

--- 7. prototype/prototype.js
+++ 7. prototype/prototype.js
@@ -1,20 +1,17 @@
-function Person(name, first, second, third) {
+function Person(name, first, second) {
     this.name = name;
     this.first = first;
     this.second = second;
-    this.third = third;
-    this.sum = function() {
-        return this.first + this.second + this.third;
-    }
 }
 
-var kim = new Person('kim', 10, 20, 30);
+Person.prototype.sum = function() {
+    return 'protoype : ' + (this.first + this.second);
+}
+
+var kim = new Person('kim', 10, 20);
 kim.sum = function() {
-    return 'modified : ' + (this.first + this.second);
+    return 'this : ' + (this.first + this.second);
 }
-var lee = new Person('lee', 10, 10, 10);
-lee.sum = function() {
-    return 'modified : ' + (this.first + this.second);
-}
+var lee = new Person('lee', 10, 10);
 console.log("kim.sum()", kim.sum());
 console.log("lee.sum()", lee.sum());
